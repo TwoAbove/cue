bf568d6ab49a004086c05962d667b255e951acc5
Seva Maltsev <me@seva.dev>
2025-10-26 21:07:03 -0400

    Added coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,10 @@ export default defineConfig({
     testTimeout: 1000,
     coverage: {
       provider: "v8",
-      reporter: ["text"],
+      reporter: ["text", "text-summary", "lcov", "json-summary", "html"],
+      reportsDirectory: "./coverage",
       include: ["src/**"],
+      exclude: ["src/types/**"],
     },
   },
 });
